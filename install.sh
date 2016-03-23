3e50d3912e57e465eac19a5fea95b95fd1b90de4
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-23 07:37:56 +0000

    日本語化

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 
 cd /home/ubuntu
 
-sudo apt-get install supervisor xvfb fluxbox x11vnc libnotify-dev
+sudo apt-get install supervisor xvfb fluxbox x11vnc libnotify-dev unifont
 
+#libdbusmenu-glib4 libdbusmenu-gtk4 libindicator7 fonts-liberation libappindicator1 xdg-utils language-pack-ja-base language-pack-ja
+#sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE=”ja_JP:ja”
+#source /etc/default/locale
+   
 git clone git://github.com/kanaka/noVNC
 
 # .bashrc の末尾に環境変数DISPLAYを追加
