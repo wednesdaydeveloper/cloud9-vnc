2ee86f43c509d64424493b08ef2b9e63d943e2d4
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-23 05:48:27 +0000

    .bashrc の末尾に環境変数DISPLAYを追加

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ cd /home/ubuntu
 sudo apt-get install supervisor xvfb fluxbox x11vnc libnotify-dev
 
 git clone git://github.com/kanaka/noVNC
+
+# .bashrc の末尾に環境変数DISPLAYを追加
+echo # DISPLAY >> ~/.bashrc
+echo export DISPLAY=":99" >> ~/.bashrc
