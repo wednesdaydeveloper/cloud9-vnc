5d380092a2a1ef6ba81494afa23c93a1f7dde59f
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-24 03:10:56 +0000

    webpack化

--- src/js/react/index.js
+++ src/js/react/index.js
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import { render } from 'react-dom';
 import Main from './components/view.js';
 
-ReactDOM.render(
+render(
   <Main />,
   document.getElementById('content')
 );
