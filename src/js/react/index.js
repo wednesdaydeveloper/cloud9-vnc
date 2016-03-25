2048b352285343e9d2da6f08ad688e4a0792b041
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-25 05:09:11 +0000

    mysql

--- src/js/react/index.js
+++ src/js/react/index.js
@@ -9,14 +9,23 @@ render(
 
 
 
-const Sequelize = require("sequelize");
+var Sequelize = require('sequelize');
+var IP = process.env.IP;
+var username = process.env.C9_USER;
+var password = null;
+var sequelize = 
+  new Sequelize('c9', username, password, {
+    host: IP,
+    port: 3306,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+  });
+  
 const Model = require("../db/models");
-
-const sequelize = new Sequelize('database', '', '', {
-  dialect: 'sqlite',
-  storage: './sequelize-sqlite3.db'
-});
-
 const User = Model.User(sequelize);
 
 User.findAll().then(users => {
